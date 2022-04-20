72d415d95d762516535e0c299a70e6808ea708ac
JLamp <jlampron@wistia.com>
2022-04-20 00:30:34 -0400

    add new button component

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,35 +1,135 @@
 import styled, { css } from "styled-components";
+import { Icon } from "./Icon/Icon";
+import { shade, tint } from "polished";
 import { Link } from "react-router-dom";
 
-const ButtonStyles = css`
+const baseButtonStyles = css`
   all: unset;
   display: flex;
+  flex-direction: row;
+  font-weight: 700;
   justify-content: center;
   align-items: center;
-  padding: 12px;
-  background: ${({ theme }) => theme.colors.ink};
+  aspect-ratio: ${({ children }) => !children && "1 / 1"};
+`;
+
+const primaryButtonStyles = css`
+  background-color: ${({ theme }) => theme.colors.ink};
   color: ${({ theme }) => theme.colors.paper};
-  width: 100%;
-  cursor: pointer;
-  font-weight: 700;
-  font-size: 24px;
   &:hover {
-    background: #3b3b3b;
+    background-color: ${({ theme }) => tint(0.15, theme.colors.ink)};
   }
 `;
 
+const secondaryButtonStyles = css`
+  color: ${({ theme }) => theme.colors.ink};
+  background-color: ${({ theme }) => theme.colors.paper};
+  border: 1px solid ${({ theme }) => theme.colors.ink};
+  &:hover {
+    background-color: ${({ theme }) => shade(0.15, theme.colors.paper)};
+  }
+`;
+
+const tertiaryButtonStyles = css`
+  ${secondaryButtonStyles};
+  border: none;
+`;
+const largeButtonStyles = css`
+  font-size: 24px;
+  gap: 16px;
+  padding: 8px 16px;
+`;
+
+const smallButtonStyles = css`
+  font-size: 16px;
+  gap: 8px;
+  padding: 4px 8px;
+`;
+
 const StyledButton = styled.button`
-  ${ButtonStyles}
+  ${baseButtonStyles}
+  ${({ variant }) => {
+    switch (variant) {
+      case "primary":
+        return primaryButtonStyles;
+      case "secondary":
+        return secondaryButtonStyles;
+      case "tertiary":
+        return tertiaryButtonStyles;
+    }
+  }}
+  ${({ size }) => {
+    switch (size) {
+      case "large":
+        return largeButtonStyles;
+      case "small":
+        return smallButtonStyles;
+    }
+  }}
 `;
 
 const StyledLink = styled(Link)`
-  ${ButtonStyles}
+  ${baseButtonStyles}
+  ${({ variant }) => {
+    switch (variant) {
+      case "primary":
+        return primaryButtonStyles;
+      case "secondary":
+        return secondaryButtonStyles;
+      case "tertiary":
+        return tertiaryButtonStyles;
+    }
+  }}
+  ${({ size }) => {
+    switch (size) {
+      case "large":
+        return largeButtonStyles;
+      case "small":
+        return smallButtonStyles;
+    }
+  }}
 `;
 
-export function ButtonLink({ to, children }) {
-  return <StyledLink to={to}>{children}</StyledLink>;
+export function Button({ icon, size, variant, children, onClick }) {
+  const iconSize = () => {
+    switch (size) {
+      case "large":
+        return "lg";
+      case "small":
+        return "md";
+    }
+  };
+  return (
+    <StyledButton onClick={onClick} size={size} variant={variant}>
+      {icon && <Icon type={icon} size={iconSize()} />}
+      {children}
+    </StyledButton>
+  );
 }
 
-export function Button({ onClick, children }) {
-  return <StyledButton onClick={onClick}>{children}</StyledButton>;
+export function ButtonLink({ icon, size, variant, children, to }) {
+  const iconSize = () => {
+    switch (size) {
+      case "large":
+        return "lg";
+      case "small":
+        return "md";
+    }
+  };
+  return (
+    <StyledLink to={to} size={size} variant={variant}>
+      {icon && <Icon type={icon} size={iconSize()} />}
+      {children}
+    </StyledLink>
+  );
 }
+
+Button.defaultProps = {
+  size: "large",
+  variant: "primary",
+};
+
+ButtonLink.defaultProps = {
+  size: "large",
+  variant: "primary",
+};
--- src/routes/Guide.jsx
+++ src/routes/Guide.jsx
@@ -1,11 +1,8 @@
 import { Outlet, Link } from "react-router-dom";
-import styled from "styled-components";
-import NavBar from "../components/NavBar";
 
 export default function Guide() {
   return (
     <>
-      <NavBar />
       <Outlet />
     </>
   );
